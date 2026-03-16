0108f47c6b732c5f0998854f1d85ca9b4781242e
Endryo <endryo220501@edu.unipar.br>
2026-03-16 20:12:45 -0300

    v2

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from 'react'
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native"
 
 export default function App() {
+  
+  const [numero, setNumero] = useState(0)
+
+  const aumentarNumero = () => {
+    setNumero(numero + 1)
+  }
+  const diminuirNumero = () => {
+    setNumero(numero - 1)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Image source={require('./assets/logounipar.png')} 
+        style={{
+          width: 400,   
+          height: 200,    
+          resizeMode: 'contain', 
+          position: 'absolute', 
+          top: 50,
+        }}
+      /> 
+      
+      <Text style={styles.textoUm}>Contador Truco </Text>
+      <Text>{numero}</Text>
+      <View style={styles.containerBotoes}>
+        <Button title="+" color={"#33ff00"} onPress={aumentarNumero} />
+        <Button title="-" color={"#ff0000"} onPress={diminuirNumero} />
+      </View>
+      <StatusBar style="auto" />  
+      
+
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textoUm:{
+    fontSize:35,
+    marginTop:-200
+  },
+  containerBotoes: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', // Cria um espaço entre eles
+    marginTop: 20, // Distância entre o número e os botões
+    width: '20%', // Define uma largura para o grupo de botões não ficar grudado
   },
 });
